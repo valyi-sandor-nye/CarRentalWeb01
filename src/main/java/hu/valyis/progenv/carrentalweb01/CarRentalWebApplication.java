3feb386c94eef2041945b327396ef4f66a36c025
valyi-sandor-nye <urugvalyi@gmail.com>
2025-03-18 21:33:27 +0100

    Initial commit, made by spring initializr

---
+++ src/main/java/hu/valyis/progenv/carrentalweb01/CarRentalWebApplication.java
@@ -0,0 +1,13 @@
+package hu.valyis.progenv.carrentalweb01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentalWebApplication.class, args);
+	}
+
+}
