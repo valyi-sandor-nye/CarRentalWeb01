84e285c91d46c2e8576b16db0409572f9a5d4d35
valyi-sandor-nye <urugvalyi@gmail.com>
2025-03-25 09:25:47 +0100

    CarController, CustomerController and RentalEventController is added, with 1-1 end pointt for general R(ead) operations

--- src/main/java/hu/valyis/progenv/carrentalweb01/controller/RentalEventController.java
+++ src/main/java/hu/valyis/progenv/carrentalweb01/controller/RentalEventController.java
@@ -1,7 +1,7 @@
 package hu.valyis.progenv.carrentalweb01.controller;
 
-import hu.valyis.progenv.carrentalweb01.model.Book;
-import hu.valyis.progenv.carrentalweb01.service.BookService;
+import hu.valyis.progenv.carrentalweb01.model.RentalEvent;
+import hu.valyis.progenv.carrentalweb01.service.RentalEventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,14 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController()
-@RequestMapping("books")
-public class BookController {
+@RequestMapping("rentalevents")
+public class RentalEventController {
 
     @Autowired
-    private BookService bookService;
+    private RentalEventService rentalEventService;
 
     @GetMapping()
-    public List<Book> getAllBooks() {
-        return bookService.getAllBooks();
-    }
+    public List<RentalEvent> getAllRentalEvents()
+    {return rentalEventService.getAllRentalEvents(); }
+
 }
